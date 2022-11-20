e9b340b5dade7ddb82c0f20d111d900d44758be8
Jake Correnti <jakecorrenti+github@proton.me>
2022-11-20 12:22:21 -0500

    restarting neetcode 150. finished contains duplicate problem

--- easy/contains-duplicates/main.go
+++ easy/contains-duplicates/main.go
@@ -1,12 +1,12 @@
 package main
 
 func containsDuplicate(nums []int) bool {
-	dict := make(map[int]int)
-	for _, num := range nums {
-		dict[num] = dict[num] + 1
-		if dict[num] > 1 {
+	occur := make(map[int]bool, 0)
+	for _, n := range nums {
+		if _, ok := occur[n]; ok {
 			return true
 		}
+		occur[n] = true
 	}
 	return false
 }
