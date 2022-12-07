6b3cf87bc65f1e89292b9291213e113c8f22ff1d
Jake Correnti <jakecorrenti+github@proton.me>
2022-12-06 19:15:00 -0500

    removed unnecessary comments

--- medium/add-two-numbers/main.go
+++ medium/add-two-numbers/main.go
@@ -7,8 +7,6 @@ type ListNode struct {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	// can start at beginning of each, add, then keep track of any carry somehow (if x / 10 > 1, then there's a carry), and when reach the end of the nodes, add the carry node if necessary.
-
-	// need to make sure to check if one list is done but not the other.
 	carry := 0
 	var head *ListNode = nil
 	tmpHead := head
@@ -27,17 +25,17 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			sum += 1
 			carry = 0
 		}
+
 		if sum/10 >= 1 {
 			carry = 1
 			sum -= 10
 		}
 
-		// currently determined the sum of the two nodes and if there is a carry necessary
-		// need to do a tail insert on the current running list
 		node := &ListNode{
 			Val:  sum,
 			Next: nil,
 		}
+
 		if head == nil {
 			head = node
 			tmpHead = head
@@ -53,8 +51,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			l2 = l2.Next
 		}
 
-		// doesn't handle the case where there is more nodes on one list than another
-		// also doesn't handle the case where we have to carry on the last val very well either
 		if l1 == nil && l2 == nil && carry > 0 {
 			node := &ListNode{
 				Val:  carry,
@@ -63,8 +59,6 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			tmpHead.Next = node
 			tmpHead = node
 		}
-
-		// need to handle the fact of resetting the carry afterwards and we don't need it anymore
 	}
 	return head
 }
