8eada480bd32f04fec783cdf75fd43283d195c92
Jake Correnti <jakecorrenti+github@proton.me>
2022-12-07 22:22:52 -0500

    reverse integer solution. beats 100% in runtime and 84% in memory usage

--- medium/add-two-numbers/main.go
+++ medium/add-two-numbers/main.go
@@ -10,12 +10,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	carry := 0
 	var head *ListNode = nil
 	tmpHead := head
+	// check to make sure that neither l1 and l2 are nil
 	for l1 != nil || l2 != nil {
 		var sum int
 
+		// if only l2 is a valid node, then that is going to be the sum
 		if l1 == nil && l2 != nil {
 			sum = l2.Val
-		} else if l1 != nil && l2 == nil {
+		} else if l1 != nil && l2 == nil { // if l1 is the only valid node, then that is going to be the sum
 			sum = l1.Val
 		} else {
 			sum = l1.Val + l2.Val
@@ -26,6 +28,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			carry = 0
 		}
 
+		// if the sum is greater than 9, then there is a carry
 		if sum/10 >= 1 {
 			carry = 1
 			sum -= 10
@@ -51,6 +54,7 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			l2 = l2.Next
 		}
 
+        // if at the end of both lists there is still a carry, add an additional node at the end for the carry
 		if l1 == nil && l2 == nil && carry > 0 {
 			node := &ListNode{
 				Val:  carry,
